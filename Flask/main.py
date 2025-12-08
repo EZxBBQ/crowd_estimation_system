cc91e0b59ef0f58b266c8bf021f08b3fbf430dfb
Adi Nugroho <adi.nugroho.59162@gmail.com>
2025-12-08 12:21:05 +0700

    central node can send json to external device

--- Flask/main.py
+++ Flask/main.py
@@ -21,19 +21,13 @@ def mainPage():
 
 @app.route("/api/systems/<int:system_id>", methods=["PATCH"])
 def update_system(system_id):
-    """
-    Expected JSON body: {
-        "crowdLevel": "LOW"|"MEDIUM"|"DENSE"|"FULL",
-        "peopleCount": int,
-        "maxPeople": int
-    }
-    """
     system = next((s for s in Systems if s["id"] == system_id), None)
     
     if not system:
         return jsonify({"error": "System not found"}), 404
     
     data = request.get_json()
+    print("payload:", data)
     
     # Update allowed fields only
     if "crowdLevel" in data:
@@ -64,4 +58,4 @@ def get_systems():
     return jsonify(Systems), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- firmware/central_node/src/central_node.cpp
+++ firmware/central_node/src/central_node.cpp
@@ -4,14 +4,22 @@
  */
 #include <Arduino.h>
 #include <painlessMesh.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
 
 // --- KONFIGURASI MESH (Harus sama persis dengan node lain) ---
 #define   MESH_PREFIX     "CrowdMesh"
 #define   MESH_PASSWORD   "meshpassword"
 #define   MESH_PORT       5555
+#define   SYSTEM_ID       0x01 
 
+const char* AP_ssid = "ClientNode_AP";
+const char* AP_password = "clientnodepass";
+const char* external_ip = "10.78.49.2";
+const uint16_t flask_port = 5000;
 Scheduler userScheduler;
 painlessMesh mesh;
+HTTPClient http;
 
 // Callback saat terima data dari Mesh (Sensor/Kamera)
 void receivedCallback( uint32_t from, String &msg ) {
@@ -19,14 +27,74 @@ void receivedCallback( uint32_t from, String &msg ) {
   // Proses data di sini (misal: simpan, tampilkan, dsb)
 }
 
+void sendDataTest(void* params)
+{
+  while (true)
+  {
+    // kirim data (dummy) ke flask server
+    String dummyImage = "dummy_link";
+    int peopleCount = 50;
+
+    String jsonBody = "{";
+    jsonBody += "\"image\":\"" + dummyImage + "\",";
+    jsonBody += "\"peopleCount\":" + String(peopleCount);
+    jsonBody += "}";
+
+    if (WiFi.getMode() == WIFI_AP || true) {
+
+      String url = String("http://") + external_ip + ":" + String(flask_port) + "/api/systems/" + String(SYSTEM_ID);
+
+      Serial.println("POST → " + url);
+      Serial.println("Payload: " + jsonBody);
+
+      http.begin(url);
+      http.addHeader("Content-Type", "application/json");
+
+      int httpResponseCode = http.sendRequest("PATCH", jsonBody);
+
+      Serial.print("HTTP Response Code: ");
+      Serial.println(httpResponseCode);
+
+      if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println("Server Response: " + response);
+      } else {
+        Serial.println("HTTP POST FAILED");
+      }
+
+      http.end();
+    } 
+    else {
+      Serial.println("WiFi not in AP mode — cannot reach laptop");
+    }
+    vTaskDelay(pdTICKS_TO_MS(10000)); // delay 10 detik
+  }
+}
+
 void setup() {
   Serial.begin(115200);
+
+  // Setup AP WiFi
+  WiFi.mode(WIFI_AP);
+
   // Setup Mesh
   mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  WiFi.softAP(AP_ssid, AP_password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.println("Central Node AP IP address: " + IP.toString());
   mesh.onReceive(&receivedCallback);
   mesh.setRoot(true);
   mesh.setContainsRoot(true);
+
+  xTaskCreate(
+    sendDataTest,
+    "SendDataTask",
+    4096,
+    NULL,
+    1,
+    NULL
+  );
 }
 
 void loop() {
